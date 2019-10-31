f73db4120fac8fe30cc1e9c24f17a035924f9716
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2019-10-31 13:55:15 -0400

    Added skills and dropdown menu

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.dropdown-trigger').dropdown();
+    $('.collapsible').collapsible();
     $('.scrollspy').scrollSpy();
     $('.modal').modal();
 })
